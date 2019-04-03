19b1682c28f35396cddf05ee7fcf2d2774830071
EsercitazioneControlli <49245346+EsercitazioneControlli@users.noreply.github.com>
2019-04-03 18:55:46 +0200

    Born

---
+++ include/DirectGeometry.m
@@ -0,0 +1,11 @@
+%% DirectGeometry Function 
+% inputs: 
+% - q : current links position;
+% - biTri: transformation matrix from link i-1 to link i for qi=0; 
+% - jointType: 0 for revolute, 1 for prismatic;
+% output:
+% biTei : transformation matrix from link i-1 to link i for the input qi.
+
+function biTei = DirectGeometry(qi, biTri, linkType)
+%TODO
+end
---
+++ include/GetTransformationWrtBase.m
@@ -0,0 +1,10 @@
+%% GetTransformatioWrtBase function
+% inputs:
+% - biTei: vector of matrices containing the transformation matrices from link i-1 to link i for the current q.
+% - linkNumber: for which computing the transformation matrix;
+% output:
+% - bTi: transformation matrix from the manipulator base to the ith joint in the configuration identified by biTei.
+
+function [bTi] = GetTransformationWrtBase(biTei, linkNumber)
+%TODO
+end
